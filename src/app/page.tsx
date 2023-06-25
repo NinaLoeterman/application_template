c00b109f4cfb3d8de7c7faf1ef1f968b6d7fa936
Nina Loeterman <nina.loeterman@covver.io>
2023-06-25 18:01:54 +0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
-        <Button onClick={() => alert('hello!')}>Hello im a chakra button</Button>
+        <Button styles={{bg: 'frost.200'}} onClick={() => alert('hello!')}>Hello im a chakra button</Button>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
